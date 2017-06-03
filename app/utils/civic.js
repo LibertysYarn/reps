49c447c67cd1b4551ff8ab74b6ac2040a61d2f2b
Liberty Montano <code@libertysyarn.com>
2017-06-02 21:30:49 -0400

    civic api

--- app/utils/civic.js
+++ app/utils/civic.js
@@ -1,4 +1,23 @@
-
-
-
 var axios = require('axios');
+
+let civic_key=CIVIC;
+let params = "?key=" + civic_key;
+let address = '&address=' + user_address;
+let levels = '&levels=' + user_levels;
+
+
+function getProfile (address) {
+  return axios.get('https://www.googleapis.com/civicinfo/v2/representatives' + params + address + '&includeOffices=true' + levels);
+    .then(function (rep) {
+      return rep.data;
+    });
+}
+
+function getElections() {
+	return axios.get('https://www.googleapis.com/civicinfo/v2/elections?' + params);
+	.then(function(elections) {
+		return elections.name;
+});
+}
+
+
